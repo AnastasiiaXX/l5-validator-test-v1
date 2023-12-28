a6e61276935f46e9bade2263c7690e8d61efe1ac
AnastasiiaXX <117952341+AnastasiiaXX@users.noreply.github.com>
2023-12-28 09:20:53 +0300

    Update Validator.js

--- src/Validator.js
+++ src/Validator.js
@@ -18,3 +18,66 @@ export default class Validator {
     return new ObjectSchema();
   }
 }
+/* export default class Validator {
+  number() {
+    return {
+      isValid(value) {
+        return typeof value === 'number';
+      },
+      integers() {
+        return {
+          isValid(value) {
+            return Number.isInteger(value);
+          },
+        };
+      },
+    };
+  }
+
+  array() {
+    return {
+      isValid(value) {
+        return Array.isArray(value);
+      },
+      integers() {
+        return {
+          isValid(value) {
+            if (!Array.isArray(value)) {
+              return false;
+            }
+            return value.every((element) => Number.isInteger(element));
+          },
+        };
+      },
+      custom(callback) {
+        return {
+          isValid(value) {
+            if (!Array.isArray(value)) {
+              return false;
+            }
+            return value.every(callback);
+          },
+        };
+      },
+    };
+  }
+
+  object() {
+    return {
+      shape(schema) {
+        return {
+          isValid(value) {
+            if (typeof value !== 'object' || Array.isArray(value) || value === null) {
+              return false;
+            }
+            return Object.entries(schema).every(([key, validator]) => {
+              const propertyValue = value[key];
+              return validator && validator.isValid(propertyValue);
+            });
+          },
+        };
+      },
+    };
+  }
+}
+*/
